b88523a5bf2c69973cb6285905b063f5833f8c85
aldogayala14 <aldo.g.ayala@gmail.com>
2022-02-13 21:19:09 -0300

    Third commit. Modelado de base de datos mediante JPA

---
+++ src/main/java/com/alkemy/disney/Entity/GeneroEntity.java
@@ -0,0 +1,23 @@
+package com.alkemy.disney.Entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genero")
+@Getter
+@Setter
+public class GeneroEntity {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+    private String imagen;
+
+}
