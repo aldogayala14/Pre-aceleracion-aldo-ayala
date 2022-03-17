356f7ba272043c9694a1adf5a5cf9ecfc98c7b3b
aldogayala14 <aldo.g.ayala@gmail.com>
2022-03-17 20:17:38 -0300

    3 commit. Creacion de Dtos y endpoints en Postman

--- src/main/java/com/alkemy/disney/controller/MovieCharacterController.java
+++ src/main/java/com/alkemy/disney/controller/MovieCharacterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("personajes")
+@RequestMapping("characters")
 public class MovieCharacterController {
 
     @Autowired
