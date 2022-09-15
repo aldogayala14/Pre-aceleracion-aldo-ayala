9707e5010cb94697d3724a4c869de7280fef664a
aldogayala14 <aldo.g.ayala@gmail.com>
2022-09-15 16:04:32 -0300

    Upload collection de Postman en documentacion

---
+++ src/test/java/com/alkemy/disney/auth/controller/UserAuthControllerTest.java
@@ -0,0 +1,20 @@
+package com.alkemy.disney.auth.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+
+@WebMvcTest(UserAuthController.class)
+class UserAuthControllerTest {
+
+    @Test
+    void singUp() {
+    }
+
+    @Test
+    void singIn() {
+
+    }
+}
--- src/test/java/com/alkemy/disney/controller/GenderControllerTest.java
+++ src/test/java/com/alkemy/disney/controller/GenderControllerTest.java
@@ -1,21 +1,65 @@
 package com.alkemy.disney.controller;
 
+
+
 import com.alkemy.disney.dto.GenderDTO;
-import org.junit.jupiter.api.Assertions;
+import com.alkemy.disney.entity.GenderEntity;
+import com.alkemy.disney.mapper.GenderMapper;
+import com.alkemy.disney.service.GenderService;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
+
+
+@WebMvcTest(GenderController.class)
 class GenderControllerTest {
-    private GenderController controller = new GenderController();
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private GenderMapper genderMapper;
+
+    @MockBean
+    private GenderService genderService;
+
 
     @Test
-    void getAll() {
-        ResponseEntity<List<GenderDTO>> response = controller.getAll();
+    void getAll() throws Exception {
+        GenderEntity drama = new GenderEntity();
+        drama.setId(1L);
+        drama.setName("drama");
+        drama.setImage("/img/drama.jpg");
+        drama.setDeleted(false);
+
+        GenderDTO dramaDTO = this.genderMapper.genderEntity2DTO(drama);
+
+        List<GenderDTO> listGender =
+                Arrays.asList(dramaDTO);
+
+        when(genderService.getAllGenders()).thenReturn(listGender);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/patient")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect((ResultMatcher) jsonPath("$[0].name", is("drama")));
 
     }
 
